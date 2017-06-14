334c56a576e368be98011e55f470fe335051d0af
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-06-14 10:43:31 +0700

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+// @flow
+const sum = (arr: Array<number>)  => (arr.reduce((sum, item) => sum + item, 0): number);
+
+let myStr: string;
+let myNumber: number;
+
+myStr = sum([1,2,3]);
+myNumber = sum([2,3,4]);
